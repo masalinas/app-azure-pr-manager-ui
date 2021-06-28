614009f4df370cc5e1f76d2034bf536158a76ec9
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2021-06-28 20:30:35 +0200

    last update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { TRISTATECHECKBOX_VALUE_ACCESSOR } from 'primeng/tristatecheckbox';
 import { map } from 'rxjs/operators';
 
 import { PoolRequestControllerService } from './shared/backend/api/poolRequestController.service';
+import { User } from './shared/backend/model/user';
 import { PoolRequestWithRelations } from './shared/backend/model/poolRequestWithRelations';
 
 @Component({
@@ -13,11 +15,74 @@ export class AppComponent {
   public title = 'PR List Manager';
   public loading: boolean;
   public poolRequestWithRelationss: PoolRequestWithRelations[];
+  public users: User[] = [];
+  public selectedUser: User = undefined;
 
   constructor(private poolRequestControllerService: PoolRequestControllerService) {
+    this.getUsers();  
+    //this.getPullRequests();
+  }
+
+  private getUsers() {
+    this.users.push(
+      {name: "Miguel Salinas", username: "masalinas@bilbomatica.es"},
+      {name: "Daniel Toro", username: "dtoro@bilbomatica.es"},
+      {name: "Jaime Rivero", username: "jrivero@gloin.es"},
+      {name: "Fernando Fernández Rivero", username: "ffernandez@gloin.es"},
+      {name: "Jacob Uribe Ais", username: "juribe@bilbomatica.es"},
+      {name: "Jose Dávila", username: "jadavila@gloin.es"},
+      {name: "Jose Jurado", username: "jose.jurado@twtspain.com"},
+      {name: "Fernando Pulido", username: "fpulido@gloin.es"},
+      {name: "Ricardo", username: "rflores@gloin.es"},
+      {name: "Cristian David Franco Garcia ", username: "cdfranco@bilbomatica.es"},
+      {name: "Ainara Arizaga Beistegi", username: "Ainara Arizaga Beistegi"},
+    );
+  }
+
+  private getPullRequests() {
     this.poolRequestControllerService.poolRequestControllerFind().pipe(map((datum) => datum.map((poolRequestWithRelations: any) => {
       if (poolRequestWithRelations.closedDate != undefined)
         poolRequestWithRelations.closedDate = new Date(poolRequestWithRelations.closedDate);
+
+      if (poolRequestWithRelations.targetRefName != undefined) {
+        let tokens: string[] = poolRequestWithRelations.targetRefName.split('/');
+        poolRequestWithRelations.targetRefName = tokens[tokens.length - 1];
+      }
+
+      if (poolRequestWithRelations.sourceRefName != undefined) {
+        let tokens: string[] = poolRequestWithRelations.sourceRefName.split('/');
+        poolRequestWithRelations.sourceRefName = tokens[tokens.length - 1];
+      }
+
+      return poolRequestWithRelations;
+    }))).subscribe((poolRequestWithRelationss: any) => {
+        this.poolRequestWithRelationss = poolRequestWithRelationss;
+
+        //this.loadGrid(this.machines);
+        this.loading = false;
+
+        console.log(this.poolRequestWithRelationss);
+    },
+    err => {
+      console.log(err);
+      this.loading = false;
+    });
+  }
+
+  public onUserChange(user: User) {
+    this.poolRequestControllerService.poolRequestControllerFindById(user.username).pipe(map((datum) => datum.map((poolRequestWithRelations: any) => {
+      if (poolRequestWithRelations.closedDate != undefined)
+        poolRequestWithRelations.closedDate = new Date(poolRequestWithRelations.closedDate);
+
+      if (poolRequestWithRelations.targetRefName != undefined) {
+        let tokens: string[] = poolRequestWithRelations.targetRefName.split('/');
+        poolRequestWithRelations.targetRefName = tokens[tokens.length - 1];
+      }
+
+      if (poolRequestWithRelations.sourceRefName != undefined) {
+        let tokens: string[] = poolRequestWithRelations.sourceRefName.split('/');
+        poolRequestWithRelations.sourceRefName = tokens[tokens.length - 1];
+      }
 
       return poolRequestWithRelations;
     }))).subscribe((poolRequestWithRelationss: any) => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from '@angular/common';
 import { BrowserModule,  } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule }   from '@angular/forms';
 
 // primeng dependencies
 import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
 import { TableModule } from 'primeng/table';
 
 // import angular envirotment variables
@@ -22,9 +24,11 @@ import { AppComponent } from './app.component';
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     AppRoutingModule,
     HttpClientModule,
     ButtonModule,
+    DropdownModule,
     TableModule
   ],
   declarations: [
